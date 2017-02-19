29ed3acf5df484241e2f03d3e091f3d2a600ab82
Sudarsan <isudarsanambatipudi@gmail.com>
2017-02-19 09:31:08 +0530

    Initial Commit

---
+++ src/main/java/org/codezarvis/artifactory/nexusrelease/App.java
@@ -0,0 +1,13 @@
+package org.codezarvis.artifactory.nexusrelease;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
